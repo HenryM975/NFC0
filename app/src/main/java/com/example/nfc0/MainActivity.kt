51b0d5b80ddd49ad04e2fe8c79ccf5d7cb1fd9a7
unknown <henry975@yandex.ru>
2023-01-13 19:03:15 +0300

    ...

--- app/src/main/java/com/example/nfc0/MainActivity.kt
+++ app/src/main/java/com/example/nfc0/MainActivity.kt
@@ -1,16 +1,25 @@
 package com.example.nfc0
 
+import android.app.PendingIntent
 import android.content.Intent
+import android.content.IntentFilter
 import android.nfc.*
+import android.nfc.tech.NfcA
 import android.os.Bundle
+import android.os.Trace.isEnabled
+import android.util.Log
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat.getSystemService
-
+import androidx.core.os.TraceCompat.isEnabled
 
 
 class MainActivity : AppCompatActivity() {
+
+
+    private var nfcAdapter: NfcAdapter? = null//
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -22,9 +31,14 @@ class MainActivity : AppCompatActivity() {
         } else {
             CheckNFC.setText("NFC");
         }
+
+
         val tag: Tag? = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG)
         val InfoNFC = findViewById<TextView>(R.id.InfoNFC)
         InfoNFC.setText(tag.toString())
+
+
+
 
 
         //проверка наличия и включённости
@@ -46,4 +60,62 @@ class MainActivity : AppCompatActivity() {
         */
 
     }
+
+    /*override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        if (NfcAdapter.ACTION_NDEF_DISCOVERED == intent.action) {
+            intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES)?.also { rawMessages ->
+                val messages: List<NdefMessage> = rawMessages.map { it as NdefMessage }
+                // Process the messages array
+            }
+        }
+    }*/
+    /*override fun onNewIntent(intent: Intent?)
+    {
+        super.onNewIntent(intent)
+        var tagFromIntent: Tag? = intent?.getParcelableExtra(NfcAdapter.EXTRA_TAG)
+        val nfc = NfcA.get(tagFromIntent)
+
+        val atqa: ByteArray = nfc.getAtqa()
+        val sak: Short = nfc.getSak()
+        nfc.connect()
+        val isConnected= nfc.isConnected()
+
+        if(isConnected)
+        {
+            val receivedData:ByteArray= nfc.transceive(NFC_READ_COMMAND)
+            //code to handle the received data
+            // Received data would be in the form of a byte array that can be converted to string
+            //NFC_READ_COMMAND would be the custom command you would have to send to your NFC Tag in order to read it
+        }
+        else{
+            Log.e("ans", "Not connected")
+        }
+
+    }
+    private fun enableForegroundDispatch(activity: AppCompatActivity, adapter: NfcAdapter?) {
+        val intent = Intent(activity.applicationContext, activity.javaClass)
+        intent.flags = Intent.FLAG_ACTIVITY_SINGLE_TOP
+        val pendingIntent = PendingIntent.getActivity(activity.applicationContext, 0, intent, 0)
+        val filters = arrayOfNulls<IntentFilter>(1)
+        val techList = arrayOf<Array<String>>()
+        filters[0] = IntentFilter()
+        with(filters[0]) {
+            this?.addAction(NfcAdapter.ACTION_NDEF_DISCOVERED)
+            this?.addCategory(Intent.CATEGORY_DEFAULT)
+            try {
+                this?.addDataType("text/plain")
+            } catch (ex: IntentFilter.MalformedMimeTypeException) {
+                throw RuntimeException()
+            }
+        }
+        adapter?.enableForegroundDispatch(activity, pendingIntent, filters, techList)
+    }
+    override fun onResume() {
+        super.onResume()
+        enableForegroundDispatch(this, this.nfcAdapter)
+    }*/
+
+
+
 }
